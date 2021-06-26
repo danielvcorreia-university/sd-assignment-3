c492d922d2c8d879fb61755cea2b9eeb513a49fd
danielvcorreia <danielvalacorreia@ua.pt>
2021-06-26 02:10:41 +0100

    [scripts] permissions and changed some machines that do not work

--- AirLiftVJavaRMI/src/DepartureAirportDeployAndRun.sh
+++ AirLiftVJavaRMI/src/DepartureAirportDeployAndRun.sh
@@ -1,8 +1,8 @@
 echo "Transfering data to the departure airport node."
-sshpass -f password ssh sd107@l040101-ws01.ua.pt 'mkdir -p ~/AirLift'
-sshpass -f password ssh sd107@l040101-ws01.ua.pt 'rm -rf ~/AirLift/*'
-sshpass -f password scp dirDepartureAirport.zip sd107@l040101-ws01.ua.pt:~/AirLift/
+sshpass -f password ssh sd107@l040101-ws08.ua.pt 'mkdir -p ~/AirLift'
+sshpass -f password ssh sd107@l040101-ws08.ua.pt 'rm -rf ~/AirLift/*'
+sshpass -f password scp dirDepartureAirport.zip sd107@l040101-ws08.ua.pt:~/AirLift/
 echo "Decompressing data sent to the departure airport node."
-sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd ~/AirLift ; unzip -uq dirDepartureAirport.zip'
+sshpass -f password ssh sd107@l040101-ws08.ua.pt 'cd ~/AirLift ; unzip -uq dirDepartureAirport.zip'
 echo "Executing program at the departure airport node."
-sshpass -f password ssh sd107@l040101-ws01.ua.pt 'cd ~/AirLift/dirDepartureAirport ; ./depair_com.sh sd107'
+sshpass -f password ssh sd107@l040101-ws08.ua.pt 'cd ~/AirLift/dirDepartureAirport ; ./depair_com.sh sd107'
--- AirLiftVJavaRMI/src/deployAndRunFirstTime.sh
+++ AirLiftVJavaRMI/src/deployAndRunFirstTime.sh
@@ -1,15 +1,17 @@
 xterm  -T "RMI registry" -hold -e "./RMIRegistryDeployAndRun.sh" &
-sleep 4
+sleep 10
 xterm  -T "Registry" -hold -e "./RegistryDeployAndRun.sh" &
-sleep 4
+sleep 10
 xterm  -T "General Repository" -hold -e "./GeneralReposDeployAndRun.sh" &
-sleep 2
+sleep 10
 xterm  -T "Departure Airport" -hold -e "./DepartureAirportDeployAndRun.sh" &
-sleep 2
+sleep 10
 xterm  -T "Plane" -hold -e "./PlaneDeployAndRun.sh" &
-sleep 2
+sleep 10
 xterm  -T "Destination Airport Airport" -hold -e "./DestinationAirportDeployAndRun.sh" &
-sleep 1
+sleep 10
 xterm  -T "Pilot" -hold -e "./PilotDeployAndRun.sh" &
+sleep 2
 xterm  -T "Hostess" -hold -e "./HostessDeployAndRun.sh" &
+sleep 3
 xterm  -T "Passengers" -hold -e "./PassengersDeployAndRun.sh" &
